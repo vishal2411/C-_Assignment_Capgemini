a9cdc7d9cdf9a95fb1aed0d900d0137c514cb905
vishal2411 <71117240+vishal2411@users.noreply.github.com>
2022-04-15 14:27:17 +0530

    Added Assignment 2

---
+++ Assignment2/Employee.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    internal class Employee
+    {
+
+        private int EmpNo;
+        private string EmpName;
+        private double Salary;
+	    private double HRA;
+	    private double TA;
+	    private double DA;
+	    private double PF;
+	    private double TDS;
+	    private double NetSalary;
+	    private double GrossSalary;
+
+        public Employee()
+        {
+        }
+
+        public int GetEmpNo()
+        {
+            return EmpNo;
+        }
+        public void SetEmpNo(int value)
+        {
+            this.EmpNo = value;
+        }
+        public string GetEmpName()
+        {
+            return EmpName;
+        }
+        public void SetEmpName(string value)
+        {
+            this.EmpName = value;
+        }
+        public double GetSalary()
+        {
+            return Salary;
+        }
+        public void SetSalary(double value)
+        {
+            this.Salary = value;
+        }
+
+        public double GetHra()
+        {
+            return HRA;
+        }
+        void SetHra(double value)
+        {
+            this.HRA = value;
+        }
+        public double GetTa()
+        {
+            return TA;
+        }
+        void SetTa(double value)
+        {
+            this.TA = value;
+        }
+        public double GetDa()
+        {
+            return DA;
+        }
+        void SetDa(double value)
+        {
+            this.DA = value;
+        }
+        public double GetGrossSalary()
+        {
+            return GrossSalary;
+        }
+        void SetGrossSalary(double value)
+        {
+            this.GrossSalary = value;
+        }
+        public double GetPf()
+        {
+            return PF;
+        }
+        void SetPf(double value)
+        {
+            this.PF = value;
+        }
+        public double GetTds()
+        {
+            return TDS;
+        }
+        void SetTds(double value)
+        {
+            this.TDS = value;
+        }
+        public double GetNetSalary()
+        {
+            return NetSalary;
+        }
+        void SetNetSalary(double value)
+        {
+            this.NetSalary = value;
+        }
+
+        public void calculateSalary(Employee employeedetails)
+        {
+            double hra = calculateHra(employeedetails.GetSalary());
+            double ta = calculateTa(employeedetails.GetSalary());
+            double da = calculateDa(employeedetails.GetSalary());
+            double grossSalary = employeedetails.GetSalary() + hra + ta + da;
+            double pf = .1 * grossSalary;
+            double tds = 0.18 * grossSalary;
+            double netSalary = grossSalary - (pf + tds);
+
+            employeedetails.SetHra(hra);
+            employeedetails.SetTa(ta);
+            employeedetails.SetDa(da);
+            employeedetails.SetGrossSalary(grossSalary);        
+            employeedetails.SetPf(pf);
+            employeedetails.SetTds(tds);
+            employeedetails.SetNetSalary(netSalary);
+        }
+
+        double calculateHra(double Salary)
+        {
+            if (Salary < 5000)
+            {
+                return .1 * Salary;
+            }
+            else if (Salary < 10000)
+            {
+                return .15 * Salary;
+            }
+            else if (Salary < 15000)
+            {
+                return .20 * Salary;
+            }
+            else if (Salary < 15000)
+            {
+                return .25 * Salary;
+            }
+            else
+            {
+                return .30 * Salary;
+            }
+        }
+        double calculateTa(double Salary)
+        {
+            if (Salary < 5000)
+            {
+                return .05 * Salary;
+            }
+            else if (Salary < 10000)
+            {
+                return .1 * Salary;
+            }
+            else if (Salary < 15000)
+            {
+                return .15 * Salary;
+            }
+            else if (Salary < 15000)
+            {
+                return .20 * Salary;
+            }
+            else
+            {
+                return .25 * Salary;
+            }
+        }
+        double calculateDa(double Salary)
+        {
+            if (Salary < 5000)
+            {
+                return .15 * Salary;
+            }
+            else if (Salary < 10000)
+            {
+                return .20 * Salary;
+            }
+            else if (Salary < 15000)
+            {
+                return .25 * Salary;
+            }
+            else if (Salary < 15000)
+            {
+                return .30 * Salary;
+            }
+            else
+            {
+                return .35 * Salary;
+            }
+        }
+
+    }
+}
---
+++ Assignment2/LitwareLib.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    internal class LitwareLib
+    {
+        static void Main(string[] args)
+        {
+            // Creating Object of Employee Class
+            Employee employee = new Employee();
+
+            // Variable Declaration
+            int empNo;
+            string empName;
+            double empSalary;
+
+            // Variable Initialization
+            Console.WriteLine("Enter Employee Number : ");
+            empNo = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Employee Name : ");
+            empName = Console.ReadLine();
+
+            Console.WriteLine("Enter Employee Salary: ");
+            empSalary = Convert.ToDouble(Console.ReadLine());
+
+            // Method Calling
+            employee.SetEmpNo(empNo);
+            employee.SetEmpName(empName);
+            employee.SetSalary(empSalary);
+            employee.calculateSalary(employee);
+
+            // Display Output
+            Console.WriteLine("Emp no:" + employee.GetEmpNo());
+            Console.WriteLine("Name:" + employee.GetEmpName());
+            Console.WriteLine("Salary :" + employee.GetSalary());
+            Console.WriteLine("Gross Salary :" + employee.GetGrossSalary());
+
+            Console.ReadKey();
+
+        }
+    }
+}
