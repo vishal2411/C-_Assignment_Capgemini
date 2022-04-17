0d2dd7c6441cd480e3e40a14918aa0a65b8c1b79
vishal2411 <71117240+vishal2411@users.noreply.github.com>
2022-04-18 00:06:07 +0530

    Added Assignment 6

---
+++ Assignment 6/UniCastDelegate.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniAndMultiCastDelegate
+{
+    public delegate void EmployeeDelegate(); //declaring Unicast delegate
+    public class UniCastDelegate
+    {
+
+        /*static void Main(string[] args)
+        {
+            Manager manager = new Manager();
+
+
+            EmployeeDelegate employeeDelegate = new EmployeeDelegate(manager.Display);
+
+
+            employeeDelegate.Invoke();
+
+
+        }*/
+    }
+    public class Manager
+    {
+        private int managerId;
+        private string managerName;
+        private string companyName;
+        private string designation;
+        private string workLocation;
+
+
+
+        public Manager()
+        {
+            Console.Write("Enter the manager id : ");
+            this.managerId = (Convert.ToInt32(Console.ReadLine()));
+            Console.Write("Enter the manager name : ");
+            this.managerName = Console.ReadLine();
+
+            Console.Write("Enter the Company Name : ");
+            this.companyName = Console.ReadLine();
+
+            Console.Write("Enter the manager designation : ");
+            this.designation = Console.ReadLine();
+
+            Console.Write("Enter the manager WorkLocation : ");
+            this.workLocation = Console.ReadLine();
+
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Manager Id : {0}", managerId);
+            Console.WriteLine("Manager Name : {0}", managerName);
+            Console.WriteLine("Manager Company Name : {0}", companyName);
+
+            Console.WriteLine("Manager Designation : {0}", designation);
+
+            Console.WriteLine("Manager Work Location : {0}", workLocation);
+        }
+    }
+}
---
+++ Assignment 6/WithdrawDeposit.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingDomain
+{
+    
+    public class Account
+    {
+        public int AccNumber;
+        public string CustomerName;
+        public double Balance;
+
+        public Account(int AccNumber,String CustomerName,double Balance)
+        {
+            /*this.AccNumber = (Convert.ToInt32(Console.ReadLine()));
+            this.CustomerName = Console.ReadLine() ;
+            this.Balance = Convert.ToDouble(Console.ReadLine());*/
+            this.AccNumber = AccNumber;
+            this.CustomerName = CustomerName;   
+            this.Balance = Balance; 
+        }
+       
+
+        public void Withdraw(double amount)
+        {
+            Console.WriteLine("Enter amount to withdraw");
+            Console.ReadLine();
+            //double amount;
+            //amount = Convert.ToDouble(Console.ReadLine());
+
+            if (this.Balance>500)
+            {
+                if(amount == 200 || amount == 500 || amount == 2000)
+                {
+                    Console.WriteLine(Balance + " : Rs is your Savings,Please choose Account to withdraw");
+                    Console.WriteLine("1.Savings Account");
+                    Console.WriteLine("2.Current Account");
+                    Console.Read();
+                    Console.WriteLine("Your transaction is processing...");
+                    double finalbalance = Balance - amount;
+                    Console.WriteLine(finalbalance + " : here is your balance after debiting the money, If not done by you please inform the local bank officials ");
+                    Console.WriteLine("----------THANK YOU FOR AVAILING WITRHDRAW SERVICE-----------");
+                }
+                else
+                {
+                    Console.WriteLine("Oops!! something went wrong");
+                }
+                          
+            }
+            else if(Balance < 500)
+            {
+                Console.WriteLine("Your balance is less than 500rs sorry we cannot proceed");
+            }
+            else
+            {
+                Console.WriteLine("Your Session has been Expired!!");
+            }
+            
+        }
+        public void Deposit(double amt)
+        {
+            Console.WriteLine("Enter the amount you want to deposit ");
+            Console.Read();
+            if(amt >= 500)
+            {
+                Console.WriteLine("You have entered valid number, your amount will be deposited");
+                Console.WriteLine("Thank you !! your deposition is successful");
+                double dep = Balance + amt;
+                Console.WriteLine(dep + ": Rs is your balance after the deposoition");
+                Console.WriteLine("----------THANK YOU FOR AVAILING ATM DEPOSIT SERVICE-----------");
+            }
+            else if(amt < 500)  
+            {
+                Console.WriteLine("Please deposit more than 500 Rs");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Number!!");
+            }
+        }
+    }
+    internal class WithdrawDeposit
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------WELCOME TO THE BANKING APPLICATION DOMAIN----------");
+            Console.ReadKey();
+            Account ac1 = new Account(76548,"kishan kumar",5000000);
+            ac1.Withdraw(500);
+            
+            //ac1.Deposit(500);
+        }
+    }
+}
