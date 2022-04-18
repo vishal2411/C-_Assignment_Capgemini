86da39405aa1411ea586fdd2c813d8071841a40c
vishal2411 <71117240+vishal2411@users.noreply.github.com>
2022-04-18 13:24:47 +0530

    Added Assignment 7

---
+++ Assignment7/BinarySerializer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace BinarySerializationProject
+{
+    public class Manager
+    {
+        public int managerId = 101;
+        public string managerName = "Snehal Pagare";
+        public double managerBasicSalary = 95000;
+    }
+
+
+    internal class BinarySerializer 
+    {
+        static void Main(string[] args)
+        {
+            //Binary Serialization
+            Manager manager = new Manager();
+            FileStream fileStream = new FileStream(@"c:\BinarySerialization.txt", FileMode.Create);
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(fileStream, manager); //serialization using Serialize() method
+            Console.ReadKey();
+
+            //Binary Deserialization using Deserialize() method
+            Manager managerAskingToDesialize = (Manager) formatter.Deserialize(fileStream);
+            Console.WriteLine(managerAskingToDesialize.managerId);
+            Console.WriteLine(managerAskingToDesialize.managerName);
+            Console.WriteLine(managerAskingToDesialize.managerBasicSalary);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Assignment7/Serialization.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace BinarySerializationProject
+{
+    
+    public class Employee
+    {
+        public int empId = 101;
+        public string empName = "kishan";
+        public string empDesignation = "analystA4";
+
+    }
+    public class Manager1
+    {
+        public int managerId = 1001;
+        public String managerName = "Snehal Pagare";
+        public double managerBasicPayScale = 95000;
+
+    }
+    public class MarketingExecutive
+    {
+        public int markExId = 10001;
+        public String markExName = "Johny Walker";
+        public String markExDesignation = "BDA";
+
+    }
+    internal class Serialization
+    {
+        static void Main(String[] args)
+        {
+            //Serializing Manager Object
+            Manager1 manager = new Manager1();
+            FileStream fileStream = new FileStream(@"c:\Serialization1.txt", FileMode.Create);
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(fileStream, manager);
+            Console.ReadKey();
+
+            //Serializing Employee Object
+            Employee employee = new Employee();
+            FileStream fileStream1 = new FileStream(@"c:\Serialization2.txt",FileMode.Create);
+            BinaryFormatter formatter1 = new BinaryFormatter();
+            formatter1.Serialize(fileStream, employee); 
+            Console.ReadKey();
+
+            //Serializing MarketingExecutive Object
+            MarketingExecutive marketingExecutive = new MarketingExecutive();
+            FileStream fileStream2 = new FileStream(@"c:\Serialization3.txt", FileMode.Create);
+            BinaryFormatter formatter2 = new BinaryFormatter();
+            formatter2.Serialize(fileStream2, marketingExecutive);
+            Console.ReadKey();
+
+        }
+    }
+}
