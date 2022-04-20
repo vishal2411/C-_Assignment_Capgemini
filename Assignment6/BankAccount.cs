1e53482b8f927120becaadc3fa923dbf013ac231
vishal2411 <71117240+vishal2411@users.noreply.github.com>
2022-04-20 14:41:04 +0530

    Added  Assignment 6

---
+++ Assignment6/BankAccount.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment6
+{
+    public delegate void AccountDelegate(int x);
+
+    internal class BankAccount
+    {
+        public static void Main() {
+
+            int withdrawAmt, depositAmt;
+            String option;
+            Account acc = new Account();
+
+            acc.display();
+
+            Console.WriteLine(" \n Select Option : Withdraw or Deposit");
+            option = Console.ReadLine();
+
+            if (option.Equals("Withdraw"))
+            {
+                Console.WriteLine("\nEnter Amount to be WithDrawn");
+                withdrawAmt = Convert.ToInt32(Console.ReadLine());
+
+                acc.UnderBalance += new AccountDelegate(acc.WithDraw);
+                acc.InsufficientBalance(withdrawAmt);
+            }
+
+            else if (option.Equals("Deposit"))
+            {
+                Console.WriteLine("\n Enter Amount to be Deposit : ");
+                depositAmt = Convert.ToInt32(Console.ReadLine());
+
+                acc.ZeroBalance += new AccountDelegate(acc.Deposit);
+                acc.DepositMoney(depositAmt);
+            }
+
+            else {
+                Console.WriteLine("Invalid Option");
+            }
+            Console.ReadKey();
+        }
+    }
+
+    public class Account {
+
+        private int accNo;
+        private string custName;
+        private double balance;
+        private double withdrawAmount;
+
+        public event AccountDelegate UnderBalance;
+        public event AccountDelegate ZeroBalance;
+
+        public Account()
+        {
+            Console.WriteLine("Enter Account Number : ");
+            this.accNo = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Customer Name: ");
+            this.custName = Console.ReadLine();
+
+            Console.WriteLine("Enter Initial Balance to Account: ");
+            this.balance = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public void display() {
+
+            Console.WriteLine("\n ----Bank Details ----- \n");
+            Console.WriteLine("Account Number is : " + accNo);
+            Console.WriteLine("Customer Name is : "+ custName);
+            Console.WriteLine("Current Balance is : " +balance);
+        }
+
+        public void InsufficientBalance(int x)
+        {
+            UnderBalance(x);
+        }
+
+        public void Deposit(int amount)
+        {
+            Console.Write("\nAccount Balance after deposit: " + (balance + amount));
+        }
+
+        public void WithDraw(int amount)
+        {
+
+            if (amount < balance && balance != 0)
+            {
+                Console.WriteLine(" \n Transaction Succesfull !!");
+                Console.WriteLine("Remaining Balance is " + (balance - amount));
+            }
+            else if (amount > balance && balance != 0)
+            {
+                Console.WriteLine("\n You have insufficient amount");
+                Console.WriteLine("Your Current Account Balance is " + balance + " only");
+            }
+            else
+            {
+                Console.WriteLine("\n Zero Balance :" + balance);
+            }
+        }
+        public void DepositMoney(int amount)
+        {
+            ZeroBalance(amount);
+        }
+    }
+}
---
+++ Assignment6/MulticastDelegate.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment6
+{
+
+    public delegate void MarketingExecutiveDelegate();
+    internal class MulticastDelegate
+    {
+        public static void Main(String[] args) {
+
+            MarketingExecutive marketingExec = new MarketingExecutive();
+
+            MarketingExecutiveDelegate marketingExecutiveDeleg = new MarketingExecutiveDelegate(marketingExec.printManagertDetails);
+
+            marketingExecutiveDeleg += marketingExec.printMarketingExectDetails;
+
+            marketingExecutiveDeleg.Invoke();
+
+            Console.ReadKey();
+        }
+    }
+
+    public class MarketingExecutive {
+
+        private int managerID, marketingExecID;
+        private string managerName, marketingExecExecName;
+        private double managerSalary, marketingExecSalary;
+
+        public MarketingExecutive() {
+            Console.WriteLine("\n ---- Enter Manager Details ---- \n");
+
+            Console.Write("Enter the Manager Id: ");
+            this.managerID = (Convert.ToInt32(Console.ReadLine()));
+
+            Console.Write("Enter the Manager Name : ");
+            this.managerName = Console.ReadLine();
+
+            Console.Write("Enter the Manager Salary : ");
+            this.managerSalary = Convert.ToDouble(Console.ReadLine());
+
+            //-------------Marketing Execuive Details ------------
+
+            Console.WriteLine("\n --- Enter Marketing Eecutive Details ----\n");
+
+            Console.Write("Enter the Marketing Executive Id: ");
+            this.marketingExecID = (Convert.ToInt32(Console.ReadLine()));
+
+            Console.Write("Enter the Marketing Executive Name : ");
+            this.marketingExecExecName = Console.ReadLine();
+
+            Console.Write("Enter the Marketing Executive Salary : ");
+            this.marketingExecSalary = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public void printManagertDetails()
+        {
+            Console.WriteLine("\n----- Manager Details --------\n");
+
+            Console.WriteLine("Manager Id : {0}", managerID);
+            Console.WriteLine("Manager Name : {0}", managerName);
+            Console.WriteLine("Manager Salary : {0}", managerSalary);
+        }
+
+        public void printMarketingExectDetails()
+        {
+            Console.WriteLine("\n----- Marketing Details --------\n");
+
+            Console.WriteLine("Marketing Executive Id : {0}", managerID);
+            Console.WriteLine("Marketing Executive Name : {0}", managerName);
+            Console.WriteLine("Marketing Executive Salary : {0}", managerSalary);
+        }
+
+    }
+}
